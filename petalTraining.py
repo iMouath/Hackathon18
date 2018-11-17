4a1de298d435a0798d610b881c9e26e167adab23
afauchoux <33325666+afauchoux@users.noreply.github.com>
2018-11-17 15:40:02 -0600

    almost ready for testing

--- petalTraining.py
+++ petalTraining.py
@@ -20,105 +20,109 @@ def grad(model, inputs, targets):
     loss_value = loss(model, inputs, targets)
   return loss_value, tape.gradient(loss_value, model.trainable_variables)
 
-tf.enable_eager_execution()
+if __name__ == '__main__':
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-train_filename = dir_path + "/iris_training.csv"
-test_filename = dir_path + "/iris_test.csv"
+    tf.enable_eager_execution()
 
-column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
-class_names = ['Iris setosa', 'Iris versicolor', 'Iris virginica']
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    train_filename = dir_path + "/iris_training.csv"
+    test_filename = dir_path + "/iris_test.csv"
 
-feature_names = column_names[:-1]
-label_name = column_names[-1]
-batch_size = 32
+    column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
+    class_names = ['Iris setosa', 'Iris versicolor', 'Iris virginica']
 
-train_dataset = tf.contrib.data.make_csv_dataset(
-    train_filename,
-    batch_size,
-    column_names=column_names,
-    label_name=label_name,
-    num_epochs=1)
+    feature_names = column_names[:-1]
+    label_name = column_names[-1]
+    batch_size = 32
 
-test_dataset = tf.contrib.data.make_csv_dataset(
-    test_filename,
-    batch_size,
-    column_names=column_names,
-    label_name=label_name,
-    num_epochs=1)
+    train_dataset = tf.contrib.data.make_csv_dataset(
+        train_filename,
+        batch_size,
+        column_names=column_names,
+        label_name=label_name,
+        num_epochs=1)
 
-train_dataset = train_dataset.map(pack_features_vector)
-test_dataset = test_dataset.map(pack_features_vector)
-features, labels = next(iter(train_dataset))
+    test_dataset = tf.contrib.data.make_csv_dataset(
+        test_filename,
+        batch_size,
+        column_names=column_names,
+        label_name=label_name,
+        num_epochs=1)
 
-model = tf.keras.Sequential([
-  tf.keras.layers.Dense(10, activation=tf.nn.relu, input_shape=(4,)),  # input shape required
-  tf.keras.layers.Dense(10, activation=tf.nn.relu),
-  tf.keras.layers.Dense(3)
-])
+    train_dataset = train_dataset.map(pack_features_vector)
+    test_dataset = test_dataset.map(pack_features_vector)
+    features, labels = next(iter(train_dataset))
 
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
-global_step = tf.train.get_or_create_global_step()
-loss_value, grads = grad(model, features, labels)
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(10, activation=tf.nn.relu, input_shape=(2,)),  # input shape required, based on our dataset.
+        tf.keras.layers.Dense(10, activation=tf.nn.relu),
+        tf.keras.layers.Dense(1) # Expected output
+    ])
 
-optimizer.apply_gradients(zip(grads, model.variables), global_step)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+    global_step = tf.train.get_or_create_global_step()
+    loss_value, grads = grad(model, features, labels)
 
-train_loss_results = []
-train_accuracy_results = []
+    optimizer.apply_gradients(zip(grads, model.variables), global_step)
 
-num_epochs = 201
+    train_loss_results = []
+    train_accuracy_results = []
 
-for epoch in range(num_epochs):
-  epoch_loss_avg = tfe.metrics.Mean()
-  epoch_accuracy = tfe.metrics.Accuracy()
+    num_epochs = 201
 
-  # Training loop - using batches of 32
-  for x, y in train_dataset:
-    # Optimize the model
-    loss_value, grads = grad(model, x, y)
-    optimizer.apply_gradients(zip(grads, model.variables),
-                              global_step)
+    for epoch in range(num_epochs):
+        epoch_loss_avg = tfe.metrics.Mean()
+        epoch_accuracy = tfe.metrics.Accuracy()
 
-    # Track progress
-    epoch_loss_avg(loss_value)  # add current batch loss
-    # compare predicted label to actual label
-    epoch_accuracy(tf.argmax(model(x), axis=1, output_type=tf.int32), y)
+        # Training loop - using batches of 32
+        for x, y in train_dataset:
+            # Optimize the model
+            loss_value, grads = grad(model, x, y)
+            optimizer.apply_gradients(zip(grads, model.variables),
+                                      global_step)
 
-  # end epoch
-  train_loss_results.append(epoch_loss_avg.result())
-  train_accuracy_results.append(epoch_accuracy.result())
+            # Track progress
+            epoch_loss_avg(loss_value)  # add current batch loss
+            # compare predicted label to actual label
+            epoch_accuracy(tf.argmax(model(x), axis=1, output_type=tf.int32), y)
 
-  if epoch % 50 == 0:
-    print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
-                                                                epoch_loss_avg.result(),
-                                                                epoch_accuracy.result()))
+        # end epoch
+        train_loss_results.append(epoch_loss_avg.result())
+        train_accuracy_results.append(epoch_accuracy.result())
 
-#Test the trained model
-test_accuracy = tfe.metrics.Accuracy()
+        if epoch % 50 == 0:
+            print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
+                                                                        epoch_loss_avg.result(),
+                                                                        epoch_accuracy.result()))
 
-for (x, y) in test_dataset:
-  logits = model(x)
-  prediction = tf.argmax(logits, axis=1, output_type=tf.int32)
-  test_accuracy(prediction, y)
+    # Test the trained model
+    test_accuracy = tfe.metrics.Accuracy()
 
-print("Test set accuracy: {:.3%}".format(test_accuracy.result()))
+    for (x, y) in test_dataset:
+        logits = model(x)
+        prediction = tf.argmax(logits, axis=1, output_type=tf.int32)
+        test_accuracy(prediction, y)
 
-#create single test input loop
-inputToTest = input("Please enter data to test:")
-input = inputToTest.split(',')
-input = list(map(float, iter(input)))
-print(input)
-predict_dataset2 = tf.convert_to_tensor([input])
-predict_dataset = tf.convert_to_tensor([
-    [5.1, 3.3, 1.7, 0.5,],
-    [5.9, 3.0, 4.2, 1.5,],
-    [6.9, 3.1, 5.4, 2.1]
-])
+    print("Test set accuracy: {:.3%}".format(test_accuracy.result()))
 
-predictions = model(predict_dataset)
+    # create single test input loop
+    inputToTest = input("Please enter data to test:")
+    input = inputToTest.split(',')
+    input = list(map(float, iter(input)))
+    print(input)
+    predict_dataset2 = tf.convert_to_tensor([input])
+    predict_dataset = tf.convert_to_tensor([
+        [5.1, 3.3, 1.7, 0.5, ],
+        [5.9, 3.0, 4.2, 1.5, ],
+        [6.9, 3.1, 5.4, 2.1]
+    ])
 
-for i, logits in enumerate(predictions):
-  class_idx = tf.argmax(logits).numpy()
-  p = tf.nn.softmax(logits)[class_idx]
-  name = class_names[class_idx]
-  print("Example {} prediction: {} ({:4.1f}%)".format(i, name, 100*p))
+    predictions = model(predict_dataset)
+
+    for i, logits in enumerate(predictions):
+        class_idx = tf.argmax(logits).numpy()
+        p = tf.nn.softmax(logits)[class_idx]
+        name = class_names[class_idx]
+        print("Example {} prediction: {} ({:4.1f}%)".format(i, name, 100 * p))
+
+
--- trainMultiplicationTables.py
+++ trainMultiplicationTables.py
@@ -4,32 +4,36 @@ from tensorflow import keras
 import numpy as np
 import keras.backend as K
 
+
 def binary_pred(y_true, y_pred):
     return K.round(y_pred)
 
+
 def decimalToBinary(dec, numberOfBits):
     answer = []
-    while(dec > 0):
+    while (dec > 0):
         answer.append(dec % 2)
         dec = dec // 2
     zeros = []
-    for i in range(0, numberOfBits-len(answer)):
+    for i in range(0, numberOfBits - len(answer)):
         zeros.append(0)
     return zeros + answer
+
 
 def binaryToDecimal(bin):
     place = 1
     sum = 0
-    for i in range(len(bin)-1, -1, -1):
+    for i in range(len(bin) - 1, -1, -1):
         sum += (place * bin[i])
         place *= 2
     return sum
 
+
 def genData(start, end):
     data = []
     labels = []
-    for i in range(start, end+1):
-        for j in range(0,11):
+    for i in range(start, end + 1):
+        for j in range(0, 11):
             num1Bin = decimalToBinary(i, 8)
             num2Bin = decimalToBinary(j, 8)
             answerBin = decimalToBinary(i * j, 16)
@@ -37,27 +41,29 @@ def genData(start, end):
             labels.append(answerBin)
     return np.array(data), np.array(labels)
 
+
 def generator(sequence_type):
-    #training labels
+    # training labels
     if sequence_type == 1:
         for i in range(6):
             for j in range(11):
                 yield i * j
-    #training data
+    # training data
     elif sequence_type == 2:
         for i in range(6):
             for j in range(11):
                 yield (i, j)
-    #testing labels
+    # testing labels
     elif sequence_type == 3:
-        for i in range(6,11):
+        for i in range(6, 11):
             for j in range(11):
                 yield i * j
-    #testing data
+    # testing data
     elif sequence_type == 4:
-        for i in range(6,11):
+        for i in range(6, 11):
             for j in range(11):
                 yield (i, j)
+
 
 def displayDataset(dataset, numberOfElements):
     iterator = dataset.make_one_shot_iterator()
@@ -67,24 +73,25 @@ def displayDataset(dataset, numberOfElements):
             val = sess.run(next_element)
             print(val)
 
-training_labels = tf.data.Dataset.from_generator(generator, (tf.int32), args = ([1]))
-training_labels = training_labels.repeat(50)
-#displayDataset(training_labels, 66*50)
 
-training_data = tf.data.Dataset.from_generator(generator, (tf.int32, tf.int32), args = ([2]))
+training_labels = tf.data.Dataset.from_generator(generator, (tf.int32), args=([1]))
+training_labels = training_labels.repeat(50)
+# displayDataset(training_labels, 66*50)
+
+training_data = tf.data.Dataset.from_generator(generator, (tf.int32, tf.int32), args=([2]))
 training_data = training_data.repeat(50)
 
-testing_labels = tf.data.Dataset.from_generator(generator, (tf.int32), args = ([3]))
+testing_labels = tf.data.Dataset.from_generator(generator, (tf.int32), args=([3]))
 testing_labels = testing_labels.repeat(50)
 
-testing_data = tf.data.Dataset.from_generator(generator, (tf.int32, tf.int32), args = ([4]))
+testing_data = tf.data.Dataset.from_generator(generator, (tf.int32, tf.int32), args=([4]))
 testing_data = testing_data.repeat(50)
 
-data, label = genData(1,5)
+data, label = genData(1, 5)
 train_data_ds = tf.data.Dataset.from_tensor_slices(data)
 train_label_ds = tf.data.Dataset.from_tensor_slices(label)
 
-data2, label2 = genData(6,10)
+data2, label2 = genData(6, 10)
 test_data_ds = tf.data.Dataset.from_tensor_slices(data2)
 test_label_ds = tf.data.Dataset.from_tensor_slices(label2)
 
@@ -101,5 +108,5 @@ model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='categorical_crossentropy',
               metrics=['accuracy', binary_pred])
 
-model.fit(data, label,  epochs = 10,
-          validation_data = (data2, label2)) # pass callback to training
+model.fit(data, label, epochs=10,
+          validation_data=(data2, label2))  # pass callback to training
